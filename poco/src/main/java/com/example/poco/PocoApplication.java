0617da7327af4a9f79bc72ea5ae4eec2b03ef89c
Roger <yuruojie777@163.com>
2023-03-23 21:54:47 +1100

    spring cloud added

---
+++ poco/src/main/java/com/example/poco/PocoApplication.java
@@ -0,0 +1,15 @@
+package com.example.poco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocoApplication.class, args);
+    }
+
+}
---
+++ poco/src/test/java/com/example/poco/PocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
